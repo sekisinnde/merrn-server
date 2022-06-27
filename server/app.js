19ef5b18e6c403df7ba17d6e477336f1e6f6a86e
osekisi <osekisinde@gmail.com>
2022-06-27 15:30:58 +0200

    creer bases de donnes et connecter sur mongossedb

---
+++ server/app.js
@@ -0,0 +1,21 @@
+
+const express = require('express')
+const app = express()
+const port = 3000;
+// const toysRoutes = require("./routes/toys.js");
+// const categoriesRoutes = require("./routes/categories");
+const bodyparser = require('body-parser');
+const mongoose = require('mongoose');
+app.use(bodyparser.urlencoded({extended: true}));
+app.use(bodyparser.json());
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  mongoose.connect('mongodb+srv://Matrice:QCVasMi5b2MMGwpr@cluster0.bhycl.mongodb.net/Magical-world?retryWrites=true&w=majority')
+        .then(()=>{console.log('connection à mongo resusir');})
+        .catch(()=>{console.log('connection a mongo echoué');})
+})
---
+++ server/data.js
@@ -0,0 +1,44 @@
+module.exports.toys = [
+    {
+        "name": "Playstation 4",
+        "description": "Famous video game platform",
+        "price": 499,
+        "category_id": 0
+    },
+    {
+        "name": "Barbie",
+        "description": "Pink doll",
+        "price": 29,
+        "category_id": null
+    },
+    {
+        "name": "Monopoly",
+        "description": "Board game $$$",
+        "price": 59,
+        "category_id": 1
+    },
+    {
+        "name": "Football ball",
+        "description": "A ball to play outside",
+        "price": 25,
+        "category_id": 2
+    },
+    {
+        "name": "Chess",
+        "description": "Board game for smart children",
+        "price": 25,
+        "category_id": 1
+    }
+]
+
+module.exports.categories = [
+    {
+        "name": "Videogames"
+    },
+    {
+        "name":  "Boardgames"
+    },
+    {
+        "name": "Outdoor"
+    }
+]
