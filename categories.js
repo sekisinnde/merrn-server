e5ab667a4504304626fec8fbdcdd89d6288fb3a0
osekisi <osekisinde@gmail.com>
2022-06-27 16:11:52 +0200

    Create React App && Add Back

---
+++ categories.js
@@ -0,0 +1,10 @@
+const { default: mongoose } = require('mongoose');
+ 
+// creer schema
+ const categorySchema = new mongoose.Schema({
+   //id: Number,
+   name: String
+  });
+ 
+   //mongosse model
+   module.exports = mongoose.model('Category', categorySchema)
---
+++ routes/toys.js
@@ -0,0 +1,49 @@
+           const express = require('express');
+           const { append } = require('express/lib/response');
+           const { default: mongoose } = require('mongoose');
+           const router = express.Router();
+           const Toy = require ('../modeles/toys');
+           
+           router.get('/', async(req, res) => {
+           const toys = await Toy.find()
+                .catch(()=>console.log('error'));
+                res.send(JSON.stringify(toys, null, 2));
+            });
+            
+            router.get('/:id', (req, res) => {
+                let id = req.params.id;
+                const toy =Toy.findById(id)
+                res.send(JSON.stringify(toy, null, 2));               
+            })
+                    
+            router.post('/', (req, res) =>{
+            //creer
+            const newToy = new Toy({
+                name: req.body.name,
+                price: req.body.price,
+                description: req.body.description,
+                category: req.body.category,
+            });
+            newToy.save()
+            .then(()=>console.log('enregisitrement reusit'))
+            .catch(()=>console.log('engresitrement echoué'));
+            res.send(JSON.stringify(newToy, null,2));
+           })
+            
+            router.put('/:id', async(req, res)=>{
+              
+                if (req.params.id){
+                let findtoy = await Toy.findOneAndUpdate({id: req.params.id} ,{name: req.body.name, description: req.body.description, price:req.body.price})
+                findtoy.save()
+                res.send(JSON.stringify(findtoy,null,2))
+                }else res.sendStatus(404)
+            })
+            // router.delete('/:name', function(req, res) {
+
+            //     Category.deleteOne({name:req.params.name})
+            //         .then(() => console.log("Catégorie supprimée"))
+            //         .catch(err => console.log("Erreur : Catégorie non supprimée"))
+            
+            // });
+             
+         module.exports = router;
