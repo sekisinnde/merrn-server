4548e6d2503667d313ffed2037a48ef0d9045e85
osekisi <osekisinde@gmail.com>
2022-06-28 10:31:49 +0200

    added homepage

--- client/src/Home.js
+++ client/src/Home.js
@@ -1,1 +1,42 @@
 // TODO: faire le composant avec le fetch à l'API
+
+import React, { useState, useEffect } from "react";
+import { useParams, useNavigate } from "react-router";
+ 
+const Home =() =>{
+ const [form, setForm] = useState({
+   name: "",
+   position: "",
+   level: "",
+   records: [],
+ });
+ const params = useParams();
+ const navigate = useNavigate();
+ 
+ useEffect(() => {
+   async function fetchData() {
+     const id = params.id.toString();
+     const response = await fetch(`http://localhost:3000/toys/`);
+ 
+     if (!response.ok) {
+       const message = `An error has occurred: ${response.statusText}`;
+       window.alert(message);
+       return;
+     }
+ 
+     const record = await response.json();
+     if (!record) {
+       window.alert(`Record with id ${id} not found`);
+       navigate("/");
+       return;
+     }
+ 
+     setForm(record);
+   }
+ 
+   fetchData();
+ 
+   return;
+ }, [params.id, navigate]);
+}
+export default Home
